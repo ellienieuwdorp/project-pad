73bdf172a22f9730f82f13a66b987f28c0c38508
Hamam <you@example.com>
2017-04-06 16:41:34 +0200

    improving for the rfcomm server

--- pi_code/rfcomm-server.py
+++ pi_code/rfcomm-server.py
@@ -20,21 +20,22 @@ advertise_service( server_sock, "SampleServer",
                    profiles = [ SERIAL_PORT_PROFILE ], 
 #                   protocols = [ OBEX_UUID ] 
                     )
-                   
-print("Waiting for connection on RFCOMM channel %d" % port)
-
-client_sock, client_info = server_sock.accept()
-print("Accepted connection from ", client_info)
-
-try:
-    while True:
-        data = client_sock.recv(1024)
-        if len(data) == 0: break
-        print("received [%s]" % data)
-except IOError:
-    pass
-
-print("disconnected")
+                  
+while True: 
+	print("Waiting for connection on RFCOMM channel %d" % port)
+	
+	client_sock, client_info = server_sock.accept()
+	print("Accepted connection from ", client_info)
+	
+	try:
+	    while True:
+	        data = client_sock.recv(1024)
+	        if len(data) == 0: break
+	        print("received [%s]" % data)
+	except IOError:
+	    pass
+	
+	print("disconnected")
 
 client_sock.close()
 server_sock.close()
