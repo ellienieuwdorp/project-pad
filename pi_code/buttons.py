e97b6fc52bfc92e057562d3aacde27d6563c06a0
Hamam <you@example.com>
2017-04-11 16:33:23 +0200

    program sleeps until button released

--- pi_code/buttons.py
+++ pi_code/buttons.py
@@ -1,18 +1,21 @@
 # import RPi.GPIO as GPIO
 from gpiozero import Button
-import time
+from time import sleep
 import os
 import piano
 
-buttons = [Button(5), Button(6), Button(13), Button(19), Button(11)]
+buttons = [Button(5), Button(6), Button(13), Button(19), Button(26)]
 
-collection = "Farts"
-sounds = os.listdir("collections/{:s}".format(collections))
+#collection = "Farts"
+#sounds = os.listdir("collections/{:s}".format(collections))
 
-piano.start()
+#piano.start()
 while True:
     for button in buttons:
         if button.is_pressed:
-            piano.fadeout(2000);
-            playMusic(sounds[buttons.index(button)])
+            # piano.fadeout(2000);
+            # playMusic(sounds[buttons.index(button)])
             # time.sleep(3)
+	    print(buttons.index(button))
+	    sleep(0.5)
+	    button.wait_for_release()
