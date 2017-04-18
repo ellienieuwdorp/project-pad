1b799ef37bc4803a4f767287e2627f7972185499
Luuk Nieuwdorp <luuknieuwdorp@gmail.com>
2017-04-18 13:51:02 +0200

    unfuck git

--- pi_code/rfcomm-server.py
+++ pi_code/rfcomm-server.py
@@ -20,22 +20,21 @@ advertise_service( server_sock, "SampleServer",
                    profiles = [ SERIAL_PORT_PROFILE ], 
 #                   protocols = [ OBEX_UUID ] 
                     )
-                  
-while True: 
-	print("Waiting for connection on RFCOMM channel %d" % port)
-	
-	client_sock, client_info = server_sock.accept()
-	print("Accepted connection from ", client_info)
-	
-	try:
-	    while True:
-	        data = client_sock.recv(1024)
-	        if len(data) == 0: break
-	        print("received [%s]" % data)
-	except IOError:
-	    pass
-	
-	print("disconnected")
+                   
+print("Waiting for connection on RFCOMM channel %d" % port)
+
+client_sock, client_info = server_sock.accept()
+print("Accepted connection from ", client_info)
+
+try:
+    while True:
+        data = client_sock.recv(1024)
+        if len(data) == 0: break
+        print("received [%s]" % data)
+except IOError:
+    pass
+
+print("disconnected")
 
 client_sock.close()
 server_sock.close()
