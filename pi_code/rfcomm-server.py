056ae79905b800b889022f157ef4f5e142d3b909
Hamam <you@example.com>
2017-04-20 17:38:30 +0200

    recieving collection

--- pi_code/rfcomm-server.py
+++ pi_code/rfcomm-server.py
@@ -17,24 +17,26 @@ uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 advertise_service( server_sock, "SampleServer",
                    service_id = uuid,
                    service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ], 
-#                   protocols = [ OBEX_UUID ] 
+                   profiles = [ SERIAL_PORT_PROFILE ],
+#                   protocols = [ OBEX_UUID ]
                     )
-                  
-while True: 
+
+while True:
 	print("Waiting for connection on RFCOMM channel %d" % port)
-	
+
 	client_sock, client_info = server_sock.accept()
 	print("Accepted connection from ", client_info)
-	
+
 	try:
 	    while True:
 	        data = client_sock.recv(1024)
 	        if len(data) == 0: break
 	        print("received [%s]" % data)
+            	if data.rsplit(None, 1)[0] == "collection: ":
+                	collection = data.rsplit(None, 1)[1]
 	except IOError:
 	    pass
-	
+
 	print("disconnected")
 
 client_sock.close()
