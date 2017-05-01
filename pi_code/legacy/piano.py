326efefaf0069b59fd8b86b09825f6112e8515d6
Hamam <you@example.com>
2017-05-01 22:42:33 +0200

    structure in pi_code

--- pi_code/legacy/piano.py
+++ pi_code/legacy/piano.py
@@ -1,8 +1,8 @@
 import pygame
 import time
 
-file = "test.mp3"
-def start():
+name = "test.mp3"
+def start_music():
     pygame.mixer.init()
     pygame.mixer.music.set_volume(1.0)
 
@@ -10,7 +10,7 @@ def playMusic(name):
     pygame.mixer.music.load(name)
     pygame.mixer.music.play()
 
-# num is in milliseconds 
+# num is in milliseconds
 def fadeout(num):
     pygame.mixer.music.fadeout(num)
 
