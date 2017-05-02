8c52e233c0f7179e19c0d880bf268acf4fc4b52e
Hamam <you@example.com>
2017-05-02 13:27:49 +0200

    bug fixes

--- pi_code/better/bt_client.py
+++ pi_code/better/bt_client.py
@@ -12,7 +12,7 @@ class BtClient(object):
         self.server_sock.bind(("",PORT_ANY))
         self.server_sock.listen(1)
 
-        self.port = server_sock.getsockname()[1]
+        self.port = self.server_sock.getsockname()[1]
 
         self.uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
@@ -26,23 +26,23 @@ class BtClient(object):
 
     # the most important function
     def recieving(self):
+
         while True:
-        	print("Waiting for connection on RFCOMM channel %d" % port)
+            print("Waiting for connection on RFCOMM channel %d" % self.port)
+            self.client_sock, self.client_info = self.server_sock.accept()
+            print("Accepted connection from ", self.client_info)
 
-        	client_sock, client_info = server_sock.accept()
-        	print("Accepted connection from ", client_info)
+            try:
+                while True:
+                    data = self.client_sock.recv(1024)
+                if len(data) == 0: break
+                print("received [%s]" % data)
+                if data.rsplit(None, 1)[0] == "collection: ":
+                    self.piano.set_collection(self.piano, data.rsplit(None, 1)[1])
+            except IOError:
+                pass
 
-        	try:
-        	    while True:
-        	        data = client_sock.recv(1024)
-        	        if len(data) == 0: break
-        	        print("received [%s]" % data)
-                    	if data.rsplit(None, 1)[0] == "collection: ":
-                        	self.piano.set_collection(self.piano,data.rsplit(None, 1)[1])
-        	except IOError:
-        	    pass
-
-        	print("disconnected")
+            print("disconnected")
 
 
     def close_conn(self):
--- pi_code/better/piano.py
+++ pi_code/better/piano.py
@@ -5,7 +5,7 @@
 
 import pygame
 from gpiozero import Button
-
+import os
 class Piano(object):
     """docstring for Piano."""
 
@@ -15,8 +15,8 @@ class Piano(object):
 
         self.buttons = [Button(5), Button(6), Button(13), Button(19), Button(26)]
         self.collection = "Farts"
-        self.sounds = os.listdir("collections/{:s}".format(collection))
-
+        self.dir = "collections/{:s}/".format(self.collection)
+        self.sounds = sorted(os.listdir(self.dir))
     def play_sound(self, name):
         pygame.mixer.music.load(name)
         pygame.mixer.music.play()
@@ -29,17 +29,20 @@ class Piano(object):
 
     def set_collection(self, name):
         self.collection = name
-        self.sounds = os.listdir("collections/{:s}".format(self.collection))
+        self.dir = "collections/{:s}/".format(self.collection)
+        self.sounds = sorted(os.listdir(self.dir))
 
     # The most important function
     def keys(self):
-        # foreach-loop
-        for btn in self.buttons:
-            if btn.is_pressed:
+        while True:
+            # foreach-loop
+            for btn in self.buttons:
+                if btn.is_pressed:
+                    print(self.buttons.index(btn))
                 # Sound will fade out in 2 sec, if something is being played.
-                if self.is_playing:
-                    self.fadeout(2000);
-                # play the sound that belongs to the button, using button index.
-                self.play_sound(self.sounds[self.buttons.index(btn)])
-                # stops the program until button releas
-        	    btn.wait_for_release()
+                    if self.is_playing():
+                        self.fadeout(2000);
+                    # play the sound that belongs to the button, using button index.
+                    self.play_sound(self.dir + self.sounds[self.buttons.index(btn)])
+                    # stops the program until button release
+                    btn.wait_for_release()
