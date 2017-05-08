7dce6ba8610a402536b2e61fe78ca7fa6b66c7cb
koen546 <koen_hengsdijk@live.nl>
2017-05-08 19:50:41 +0200

    een begin bestand in visio en wat comments bij de pi code

--- pi_code/better/demo.py
+++ pi_code/better/demo.py
@@ -12,19 +12,26 @@ import atexit
 from bt_client import BtClient
 from piano import Piano
 
+#an instance of the piano class is made
 piano = Piano()
+#an instance of the BtClient class is made
 bt_c = BtClient(piano)
 
+#this method closed the connection
 def exit_handler():
     print('The application is ending!')
     bt_c.close_conn()
 
 atexit.register(exit_handler)
 
+#new threads are started
 try:
+	#a thread for listening to the piano keys is started
    _thread.start_new_thread( piano.keys ,())
+   #a thread for the bluetooth connection is started
    _thread.start_new_thread( bt_c.recieving ,())
 
+#if the treads cannot be started a error message is displayed
 except:
    print ("Error: unable to start thread")
 
--- pi_code/better/piano.py
+++ pi_code/better/piano.py
@@ -9,30 +9,42 @@ import os
 class Piano(object):
     """docstring for Piano."""
 
+    #the constructor of the piano class
     def __init__(self):
+    	#the pygame mixer is initialized 
         pygame.mixer.init()
+        #the volume of the mixer is set to 1
         pygame.mixer.music.set_volume(1.0)
-
+        #the gpio pins of the buttons are defined
         self.buttons = [Button(5), Button(6), Button(13), Button(19), Button(26)]
+        #the name of the collection used
         self.collection = "Farts"
+
         self.dir = "collections/{:s}/".format(self.collection)
+
         self.sounds = sorted(os.listdir(self.dir))
+
+    #this method plays a sound    
     def play_sound(self, name):
         pygame.mixer.music.load(name)
         pygame.mixer.music.play()
-
+    #this method fades out the sound over a specified amount of time
     def fadeout(self, num):
         pygame.mixer.music.fadeout(num)
 
+    #this method checks if there is any sound playing
     def is_playing(self):
         return pygame.mixer.music.get_busy()
 
+    #this method sets a new collection
     def set_collection(self, name):
         self.collection = name
         self.dir = "collections/{:s}/".format(self.collection)
         self.sounds = sorted(os.listdir(self.dir))
 
-    # The most important function
+
+    #this method wait for the keys to be pressed and then plays
+    #the sound associated with key and the collection used
     def keys(self):
         while True:
             # foreach-loop
