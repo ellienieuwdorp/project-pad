aa9a1189d06e82fac6cb08e6bb4aeef3ae9adf18
Hamam <you@example.com>
2017-05-18 00:19:53 +0200

    implemented a button to make the pi discoverable

---
+++ pi_code/better/discoverable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# enable the bluetooth interface
+sudo hciconfig hci0 up
+sudo hciconfig hci0 piscan
+
+# The device will be discoverable for 60 sec
+sleep 60
+
+# disable the bluetooth interface
+sudo hciconfig hci0 noscan 
+sudo hciconfig hci0 down
